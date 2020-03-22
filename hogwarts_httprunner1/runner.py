4f135e3c987626868a4c1a3b7fada346f56b7a1e
xiaoyu0407 <vivian@tanfangdeMacBook-Air.local>
2020-03-23 00:08:41 +0800

    feat: more api

--- hogwarts_httprunner1/runner.py
+++ hogwarts_httprunner1/runner.py
@@ -4,7 +4,14 @@ from hogwarts_httprunner1.loader import load_yaml
 
 def run_yaml(yaml_file):
     load_json = load_yaml(yaml_file)
-    method = load_json.pop("method")
-    url = load_json.pop("url")
-    resp = requests.request(method, url, **load_json)
-    return resp
+    request = load_json["request"]
+    method = request.pop("method")
+    url = request.pop("url")
+    resp = requests.request(method, url, **request)
+    validator_mapping = load_json["validate"]
+    for key in validator_mapping:
+        actual_value = getattr(resp, key)
+        expected_value = validator_mapping[key]
+        assert  actual_value == expected_value
+
+    return True
--- tests/test_single_api.py
+++ tests/test_single_api.py
@@ -2,6 +2,8 @@ import os
 import unittest
 
 from hogwarts_httprunner1.loader import load_yaml
+from hogwarts_httprunner1.runner import run_yaml
+
 
 class TestSingleAPI(unittest.TestCase):
 
@@ -12,9 +14,18 @@ class TestSingleAPI(unittest.TestCase):
         """
         single_api_yaml = os.path.join(os.path.dirname(__file__),"api","get_homepage.yml")
         loaded_json = load_yaml(single_api_yaml)
-        self.assertEqual(loaded_json["url"],"https://mubu.com/")
+        self.assertIn("request",loaded_json)
+        self.assertEqual(loaded_json["request"]["url"],"https://mubu.com/")
 
     def test_run_single_api(self):
-        single_api_yaml = os.path.join(os.getcwd(), "api", "get_homepage.yml")
-        resp = run_yml(single_api_yaml)
-        self.assertEqual(resp.status_code,200)
+        single_api_yaml = os.path.join(os.path.dirname(__file__), "api", "get_homepage.yml")
+        result = run_yaml(single_api_yaml)
+        self.assertEqual(result,True)
+
+        single_api_yaml = os.path.join(os.path.dirname(__file__), "api", "api_login_submit.yml")
+        result = run_yaml(single_api_yaml)
+        self.assertEqual(result, True)
+
+        single_api_yaml = os.path.join(os.path.dirname(__file__), "api", "get_login.yml")
+        result = run_yaml(single_api_yaml)
+        self.assertEqual(result, True)
